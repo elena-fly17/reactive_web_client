15fa5d6710e41239d6e5e56eaf4b4b3f693b4c6f
elena-fly17 <elena17mr@gmail.com>
2023-07-05 15:20:26 +0300

    Успешно реализовано подключение к стороннему реактивному приложению и получение данных от него - в виде потоков Flux и Mono

--- src/main/java/com/example/reactive_web_client/ReactiveWebClientApplication.java
+++ src/main/java/com/example/reactive_web_client/ReactiveWebClientApplication.java
@@ -15,24 +15,29 @@ public class ReactiveWebClientApplication {
 		SpringApplication.run(ReactiveWebClientApplication.class, args);
 	}
 
-	// получение всех сущностей из БД
 	@Bean
 	CommandLineRunner runner() {
 		return args -> {
 
-			// указываем адрес приложения, от которого хотим получить данные
 			WebClient webClient = WebClient.create("http://localhost:8080");
 
-			// получаем данные от стороннего приложения и формируем из них поток
 			Flux<Invoice> flux = webClient
-					.get() // тип запроса
-					.uri("/invoice/allInvoices") // урл, на который отправляем запрос
-					.retrieve() // отправляем запрос и получаем ответ
-					.bodyToFlux(Invoice.class); // преобразование тело ответа в поток элементов
+					.get()
+					.uri("/invoice/allInvoices")
+					.retrieve()
+					.bodyToFlux(Invoice.class);
 
-			// выводим каждый элемент в консоль и блокируем
-			// выполнение программы, пока не получим последний элемент
 			flux.doOnNext(System.out::println).blockLast();
+			Invoice lastInvoice = flux.blockLast();
+
+			if (lastInvoice != null) {
+				webClient.get()
+						.uri("/invoice/get/{id}", lastInvoice.getId())
+						.retrieve()
+						.bodyToMono(Invoice.class)
+						.doOnSuccess(System.out::println)
+				        .block();
+			}
 		};
 	}
 
